514dc8e5453d02ff80855ddcc246f427ab3792a4
erakki27 <erakki27@gmail.com>
2025-04-27 18:05:23 +0530

    fix3
    fix3

--- Automation_AK/src/test/java/cucumberOptions/TestRunner.java
+++ Automation_AK/src/test/java/cucumberOptions/TestRunner.java
@@ -2,21 +2,23 @@ package cucumberOptions;
 
 import org.junit.runner.RunWith;
 import org.testng.annotations.DataProvider;
-import io.cucumber.testng.CucumberOptions;
 
 import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
+
 
 @RunWith(Cucumber.class)
 @CucumberOptions(  
 	    features = "src/test/java/features",
-	    glue="stepDefinations",
-	    plugin = {"pretty","html:target/cucumber.html"})
+	    glue={"stepDefinations"},
+	    plugin = {"pretty","html:target/cucumber.html", "json:target/cucumber.json"},
+monochrome = true,
+publish = true)
 public class TestRunner extends AbstractTestNGCucumberTests {
     @Override
-    //@DataProvider(parallel = true)
+    @DataProvider(parallel = true)
     public Object[][] scenarios() {
         return super.scenarios();
     }
-
 }
